dd606250b75dc259ef8a8b55c24fea2e0f68f9a8
Yulia <suzdaleva22@gmail.com>
2021-11-11 12:57:31 +0200

    Lesson15

--- js/app14.js
+++ js/app14.js
@@ -1,23 +1,20 @@
 
-
+/////////////
 let  num = 50;
-
-
-
 
 while (num <= 55) {
     console.log(num);
     num++;
 }
 
-
+///////////////////////
 let  num = 50;
 do {
     console.log(num);
     num++;
 } while (num <= 55);
 
-
+////////////////////////////
 let  num = 50;
 for (let i = 1; i < 10; i++) {
     if (i === 6) {
